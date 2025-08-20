a083e613e5db667808c3c3773722853c227866ae
Sergey Shishkin <shishkinsa1997@gmail.com>
2025-08-20 07:52:12 +0700

    feat: solve test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (!matrix || matrix.length === 0) return [];
+
+  let res = [];
+  for (let i = 0; i < matrix.length; i += 1) {
+    if (i % 2 === 0) {
+      res = res.concat(matrix[i]);
+    } else {
+      res = res.concat(matrix[i].reverse());
+    }
+  }
+
+  return res;
 };
