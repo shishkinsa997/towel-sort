d0a37df521accfa4c574c03bacd6b39bdac72c39
Sergey Shishkin <shishkinsa1997@gmail.com>
2026-04-05 14:53:18 +0500

    feat: use flatMap instead map.flat

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 module.exports = function towelSort(matrix) {
   if (!matrix || matrix.length === 0) return [];
 
-  matrix.map((x, i) => {
+  return matrix.flatMap((x, i) => {
     if (i % 2 === 0) return x;
     return x.reverse();
   });
-
-  return matrix.flat();
 };
